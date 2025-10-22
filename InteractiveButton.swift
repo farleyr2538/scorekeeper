d9820859337e69efc1caf70b8d9a5de1c5d276b3
Rob Farley <100190274+farleyr2538@users.noreply.github.com>
2025-10-22 15:18:10 +0100

    adding some button responsiveness

---
+++ InteractiveButton.swift
@@ -0,0 +1,18 @@
+//
+//  InteractiveButton.swift
+//  ScoreKeeper
+//
+//  Created by Robert Farley on 22/10/2025.
+//
+
+import SwiftUI
+
+struct InteractiveButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InteractiveButton()
+}
