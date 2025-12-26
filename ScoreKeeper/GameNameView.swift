28147aa20c9b41e30ea6e6b20cc0b57c528ce012
Rob Farley <100190274+farleyr2538@users.noreply.github.com>
2025-12-26 17:57:42 +0000

    improving design of game name textfield

--- ScoreKeeper/GameNameView.swift
+++ ScoreKeeper/GameNameView.swift
@@ -13,14 +13,19 @@ struct GameNameView: View {
     @FocusState private var isFocussed : Bool
     
     var body: some View {
-        VStack {
+        VStack(spacing: 20) {
             Text("Game Name")
                 .font(.title3.bold())
             TextField("Optional", text: $gameName)
+                
                 .padding(10)
-                .cornerRadius(10)
                 .autocorrectionDisabled()
-                .background(Color.white.opacity(0.5))
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .fill(Color.white.opacity(0.05))
+                        .stroke(Color.gray.opacity(0.5), lineWidth: 0.5)
+                )
+                
                 .focused($isFocussed)
                 .toolbar {
                     ToolbarItemGroup(placement: .keyboard) {
